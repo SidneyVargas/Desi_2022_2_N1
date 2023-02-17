01f50451c7744e9e9bdb1b60b29c437380724d4c
SidneyVargas <113731950+SidneyVargas@users.noreply.github.com>
2023-02-16 21:57:36 -0300

    final da aula

---
+++ app.js
@@ -0,0 +1,92 @@
+//console.log("hello world!");
+
+//console.log("Olá de novo!");
+
+/*let nota = 8;
+if(nota >=0 && nota <=10){
+
+    if (nota >= 7){
+        console.log("Aprovado!");
+
+
+    } else if (nota < 6) {
+        console.log("Reprovado!");
+    } else ("Recuperação!")
+} else{
+    console.log("Digite uma nota válida");
+}
+*/
+
+
+/*for(let i = 0; i <= 10; i++){
+    console.log(i)
+   
+
+}
+*/
+
+/*for(let i = 0; i <= 10; i =  i+=2){
+    console.log(i)
+}*/
+
+/*let item = "batata";
+
+switch (item) {
+    case "banana":
+        console.log("O item é uma banana!")
+         break;
+
+    case "batata":
+        console.log("O item é uma batata!")
+        break;
+
+    default:
+        console.log("O item não foi encontrado!")
+        break;
+}
+*/
+
+/*let diaDaSemana = new Date().getDay()
+//console.log(diaDaSemana);
+
+switch (diaDaSemana) {
+    case 0:
+        console.log("Domingo")
+         break;
+
+         case 1:
+         console.log("Segunda")
+          break;
+
+          case 2:
+         console.log("Terça")
+          break;
+
+          case 3:
+         console.log("Quarta")
+          break;
+
+          case 4:
+         console.log("Quinta")
+          break;
+
+          case 5:
+         console.log("Sexta")
+          break;
+
+          case 6:
+         console.log("Sábado")
+          break;
+ 
+
+    default:
+        console.log("O item não foi encontrado!")
+        break;
+}
+*/
+
+
+
+
+
+    
